0ea8da8799efb8376015a037aee3245cb3c79ef9
Nitial <emohrush@gmail.com>
2015-11-14 13:59:10 +0300

    добавили owner'а

--- EnslaverFrontEnd/Logic/AppGlobalContext.cs
+++ EnslaverFrontEnd/Logic/AppGlobalContext.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration; 
 
 namespace EnslaverFrontEnd.Logic
 {
@@ -44,6 +45,7 @@ namespace EnslaverFrontEnd.Logic
         public AppGlobalContext()
         {
 
+            Owner = ConfigurationManager.AppSettings["owner"];
         }
 
         public Type GetTypeByID(long formTypeID)
