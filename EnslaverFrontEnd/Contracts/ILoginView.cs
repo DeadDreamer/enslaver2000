3c6428c7368a3264dbc1bbbe47d5131c2b794ba7
Nitial <emohrush@gmail.com>
2015-11-14 14:45:30 +0300

    Сделали редирект на другие формы

--- EnslaverFrontEnd/Contracts/ILoginView.cs
+++ EnslaverFrontEnd/Contracts/ILoginView.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EnslaverFrontEnd.Models;
 
 namespace EnslaverFrontEnd.Contracts
 {
     public interface ILoginView : IBaseView
     {
         string GetPasswordString();
+        FormMessage GetFormMessage();
         void ShowMessage(string message);
+        void CloseView();
 
         event EventHandler<EventArgs> OkButtonClick;
     }
---
+++ EnslaverFrontEnd/Models/MessageBodyOfLoginForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnslaverFrontEnd.Models
+{
+  public   class MessageBodyOfLoginForm
+    {
+        public MessageBodyOfLoginForm(FormTypes type)
+        {
+            this.RedirectToForm= type;
+            
+        }
+
+        public FormTypes RedirectToForm { get; set; }
+    }
+}
