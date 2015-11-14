272f0806a45800010fee725da8ec78924cf52f63
severyanov <alejo.elnorte@gmail.com>
2015-11-14 12:47:15 +0300

    Воспроизведение фраз в цикле.

--- EnslaverCore/Logic/Sound/Speaker.cs
+++ EnslaverCore/Logic/Sound/Speaker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Media;
+using System.Threading;
 
 namespace EnslaverCore.Logic.Sound
 {
@@ -8,11 +9,55 @@ namespace EnslaverCore.Logic.Sound
 
         private static SoundStorage Storage = new SoundStorage();
 
-        public static void Say(string text)
+        private static void Pronounce(string text, bool sync)
         {
             Url.Text = text;
             var sound = Storage.GetSoundStream(Url.ToString());
-            new SoundPlayer(sound).Play();
+
+            var player = new SoundPlayer(sound);
+
+            if (sync)
+            {
+                player.PlaySync();
+            }
+            else
+            {
+                player.Play();
+            }
+        }
+
+        public static void Say(string text)
+        {
+            Pronounce(text, false);
+        }
+
+        public static void SaySync(string text)
+        {
+            Pronounce(text, true);
+        }
+
+        private static Timer timer;
+
+        public static void BeginSay(string text, int interval)
+        {
+            EndSay();
+
+            if (timer == null)
+            {
+                timer = new Timer((obj) =>
+                {
+                    SaySync(text);
+                }, null, 0, interval);
+            }
+        }
+
+        public static void EndSay()
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+                timer = null;
+            }
         }
     }
 }
