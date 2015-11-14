d0f9d60425434fdbd20b364ee1827c1712473877
aa.severyanov <aa.severyanov@tensor.ru>
2015-11-14 11:44:18 +0300

    Добавлено простое воспроизведение фраз.

---
+++ EnslaverCore/Logic/Sound/SoundStorage.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EnslaverCore.Logic.Sound
+{
+    public class SoundStorage
+    {
+        public SoundStorage() : this(Directory.GetCurrentDirectory()) { }
+
+        public SoundStorage(string folder)
+        {
+            this.Folder = folder;
+        }
+
+        private string Folder { get; set; }
+
+        private Dictionary<string, string> cache = new Dictionary<string, string>();
+
+        private string GetHash(string text)
+        {
+            var hash = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(text));
+            var sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+
+        private string CreateFileName(string url)
+        {
+            return Path.Combine(this.Folder, this.GetHash(url) + ".wav");
+        }
+
+        private void DownloadFile(string url, string fileName)
+        {
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(url, fileName);
+            }
+        }
+
+        public Stream GetSoundStream(string url)
+        {
+            string fileName;
+
+            url = url.ToLower();
+
+            if (!this.cache.TryGetValue(url, out fileName))
+            {
+                fileName = this.CreateFileName(url);
+
+                if (!File.Exists(fileName))
+                {
+                    this.DownloadFile(url, fileName);
+                }
+
+                this.cache.Add(url, fileName);
+            }
+
+            return File.OpenRead(this.cache[url]);
+        }
+    }
+}
---
+++ EnslaverCore/Logic/Sound/YandexSpeechKitCloudUrl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+
+namespace EnslaverCore.Logic.Sound
+{
+    public enum Emotion
+    {
+        Good,
+        Neutral,
+        Evil,
+        Mixed
+    }
+
+    public enum Voice
+    {
+        Jane,
+        Omazh,
+        Zahar,
+        Ermil
+    }
+
+    public class YandexSpeechKitCloudUrl
+    {
+        public YandexSpeechKitCloudUrl()
+        {
+            this.Speaker = Voice.Omazh;
+            this.Emotion = Emotion.Evil;
+            this.IsRobot = false;
+            this.ApiKey = "c3b2d6ef-25aa-4b3c-b2e5-a642a1f0f618";
+        }
+
+        public Emotion Emotion { get; set; }
+
+        public Voice Speaker { get; set; }
+
+        public bool IsRobot { get; set; }
+
+        public string Text { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("https://tts.voicetech.yandex.net/generate?text={0}&format=wav&speaker={1}&key={2}&emotion={3}&robot={4}",
+                HttpUtility.UrlEncode(this.Text),
+                this.Speaker.ToString().ToLower(),
+                this.ApiKey,
+                this.Emotion.ToString().ToLower(),
+                this.IsRobot.ToString().ToLower());
+        }
+    }
+}
