47cc69224140326eef175e8478c2f5968275596a
dp.puzikov <dp.puzikov@tensor.ru>
2015-11-14 17:48:39 +0300

    Прикручиваем форму

--- EnslaverFrontEnd/Presenters/AdminFormPresenter.cs
+++ EnslaverFrontEnd/Presenters/AdminFormPresenter.cs
@@ -22,57 +22,50 @@ namespace EnslaverFrontEnd.Presenters
             (this.View as IAdminView).OnTeachClick +=new EventHandler<EventArgs>(AdminFormPresenter_OnTeachClick); 
         }
 
-        Image<Bgr, Byte> currentFrame;
-        Image<Gray, byte> result = null;
-        Image<Gray, byte> gray_frame = null;
-        public CascadeClassifier face = new CascadeClassifier("haarcascade_frontalface_default.xml");
-        public CascadeClassifier eyes = new CascadeClassifier("haarcascade_eye.xml");
-        public CascadeClassifier smile = new CascadeClassifier("haarcascade_smile.xml");
-
         private void ProcessFrame(object sender, EventArgs arg)
         {
-            var capture = (this.View as IAdminView).GetCapture();
-            currentFrame = capture.QueryFrame().ToImage<Bgr, byte>().Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic);
-            gray_frame = capture.QueryFrame().ToImage<Gray, byte>().Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic);
-            gray_frame._EqualizeHist();
+            //var capture = (this.View as IAdminView).GetCapture();
+            //currentFrame = capture.QueryFrame().ToImage<Bgr, byte>().Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic);
+            //gray_frame = capture.QueryFrame().ToImage<Gray, byte>().Resize(320, 240, Emgu.CV.CvEnum.Inter.Cubic);
+            //gray_frame._EqualizeHist();
 
-            Rectangle[] facesDetected = face.DetectMultiScale(gray_frame, 1.2, 2, new Size(5, 5), new Size(320, 240));
+            //Rectangle[] facesDetected = face.DetectMultiScale(gray_frame, 1.2, 2, new Size(5, 5), new Size(320, 240));
 
-            for (int i = 0; i < facesDetected.Length; i++)
-            {
-                gray_frame.ROI = facesDetected[i];
+            //for (int i = 0; i < facesDetected.Length; i++)
+            //{
+            //    gray_frame.ROI = facesDetected[i];
 
-                Rectangle[] eyesDetected = eyes.DetectMultiScale(gray_frame, 1.2, 5, new Size(5, 5), new Size(30, 30));
-                Rectangle[] smileDetected = smile.DetectMultiScale(gray_frame, 1.1, 10, new Size(5, 5), new Size(60, 60));
+            //    Rectangle[] eyesDetected = eyes.DetectMultiScale(gray_frame, 1.2, 5, new Size(5, 5), new Size(30, 30));
+            //    Rectangle[] smileDetected = smile.DetectMultiScale(gray_frame, 1.1, 10, new Size(5, 5), new Size(60, 60));
 
-                foreach (Rectangle f in eyesDetected)
-                {
-                    f.Offset(facesDetected[i].X, facesDetected[i].Y);
-                    currentFrame.Draw(f, new Bgr(Color.Blue), 2);
-                }
+            //    foreach (Rectangle f in eyesDetected)
+            //    {
+            //        f.Offset(facesDetected[i].X, facesDetected[i].Y);
+            //        currentFrame.Draw(f, new Bgr(Color.Blue), 2);
+            //    }
 
-                if (smileDetected.Length > 0)
-                {
-                    smileDetected[0].Offset(facesDetected[i].X, facesDetected[i].Y);
-                    DrawSmile(smileDetected[0], facesDetected[i]);
-                }
+            //    if (smileDetected.Length > 0)
+            //    {
+            //        smileDetected[0].Offset(facesDetected[i].X, facesDetected[i].Y);
+            //        DrawSmile(smileDetected[0], facesDetected[i]);
+            //    }
 
-                DrawHead(facesDetected[i], i);
-            }
+            //    DrawHead(facesDetected[i], i);
+            //}
 
-            (this.View as IAdminView).SetImage(currentFrame.Resize(800, 600, Emgu.CV.CvEnum.Inter.Cubic));            
+            //(this.View as IAdminView).SetImage(currentFrame.Resize(800, 600, Emgu.CV.CvEnum.Inter.Cubic));            
         }
 
         private void DrawSmile(Rectangle smile, Rectangle head)
         {
             //currentFrame.Draw(smile, new Bgr(Color.Black), 2);
             Point loc = new Point(head.X, head.Y - 10);
-            currentFrame.Draw("smile detected", loc, FontFace.HersheyPlain, 0.7, new Bgr(Color.Yellow));
+            //currentFrame.Draw("smile detected", loc, FontFace.HersheyPlain, 0.7, new Bgr(Color.Yellow));
         }
 
         private void DrawHead(Rectangle face, int i)
         {
-            currentFrame.Draw(face, new Bgr(Color.Red), 2);
+            //currentFrame.Draw(face, new Bgr(Color.Red), 2);
         }
 
         void AdminFormPresenter_OnStopClick(object sender, EventArgs e)
